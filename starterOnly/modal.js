9b8af67cff289947fe4ef9d960ed40111fd27384
Soukaina Larabi <Soukaina.larabi44@gmail.com>
2022-12-25 15:57:18 +0100

    comnt1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -21,136 +21,124 @@ const formData = document.querySelectorAll(".formData");
 let form = document.querySelector("form")
 let modalBody = document.querySelector(".modal-body")
 let closeModal = document.querySelector(".close")
+let confirmMsgElement = document.getElementById("confirmation");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  document.forms['reserve'].reset();
+  document.forms['reserve'].reset(); //le contenue du formulaire sera initialisé
   form.style.display = "block";
   let confirmMsgElement = document.getElementById("confirmation");
   confirmMsgElement.style.display = "none";
   modalbg.style.display = "block";
 
 }
+//close Modal 
 function closeeModal() {
   modalbg.style.display = "none";
 }
-//close Modal
+//close Modal event
 closeModal.addEventListener("click", () => {
   let erreurPrenom = document.getElementById("firstNameErrorMsg")
-  // erreurPrenom.style.display = "none"
-  // let erreurNom = document.getElementById("lastNameErrorMsg")
-  // erreurNom.style.display = "none"
-  // let erreuremail = document.getElementById("emailErrorMsg")
-  // erreuremail.style.display = "none"
   modalbg.style.display = "none";
 })
-//Vérification prenom
-const prenomInvalide = (e) => {
+//Vérification Prénom
+const prenomInvalide = () => {
   let prenom = document.getElementById("first")
   let erreurPrenom = document.getElementById("firstNameErrorMsg")
   let regexPrenom = /^[a-z]{2}/gi
   if (!prenom.value) {
-    e.preventDefault()
-    erreurPrenom.innerHTML = "<font color='red'>Veuillez saisir votre prénom</font>"
+    erreurPrenom.textContent = "Veuillez saisir votre prénom"
     return false;
   }
   if (prenom.value.match(regexPrenom)) {
-    erreurPrenom.innerHTML = ""
+    erreurPrenom.textContent = ""
     return true
   }
   else {
-    e.preventDefault()
-    erreurPrenom.innerHTML = "<font color='red'>Le prénom doit contenir minimum 2 caractères </font>"
+    erreurPrenom.textContent = "Le prénom doit contenir minimum 2 caractères"
     return false
   }
 }
-//Vérification nom
-const nomInvalide = (e) => {
+//Vérification Nom
+const nomInvalide = () => {
   let nom = document.getElementById("last")
   let erreurNom = document.getElementById("lastNameErrorMsg")
   let regexNom = /^[a-z]{2}/gi
   if (!nom.value) {
-    e.preventDefault()
-    erreurNom.innerHTML = "<font color='red'>Veuillez saisir votre nom</font>"
+    erreurNom.textContent = "Veuillez saisir votre nom"
     return false
 
   }
   if (nom.value.match(regexNom)) {
-    erreurNom.innerHTML = ""
+    erreurNom.textContent = ""
     return true
   }
   else {
-    e.preventDefault()
-    erreurNom.innerHTML = "<font color='red'>Le nom doit contenir minimum 2 caractères </font>"
+    erreurNom.textContent = "Le nom doit contenir minimum 2 caractères "
     return false
 
   }
 }
-//Vérification email
-const emailInvalide = (e) => {
+//Vérification Email
+const emailInvalide = () => {
   let email = document.getElementById("email")
   let erreurEmail = document.getElementById("emailErrorMsg")
   let regexEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (!email.value) {
-    e.preventDefault()
-    erreurEmail.innerHTML = "<font color='red'>Veuillez saisir votre email</font>"
+    erreurEmail.textContent = "Veuillez saisir votre email"
     return false
   }
   if (email.value.match(regexEmail)) {
-    erreurEmail.innerHTML = ""
+    erreurEmail.textContent = ""
     return true
   }
   else {
-    e.preventDefault()
-    erreurEmail.innerHTML = "<font color='red'>L'adresse électronique est invalide </font>"
+    erreurEmail.textContent = "L'adresse électronique est invalide "
     return false
 
   }
 }
-//verification birthdate
-const birthDateInvalid = (e) => {
+//verification Birthdate
+const birthDateInvalid = () => {
   let date = document.getElementById("birthdate")
   let erreurDate = document.getElementById("birthdateErrorMsg")
   if (date.value) {
-    erreurDate.innerHTML = ""
+    erreurDate.textContent = ""
     return true
   } else {
-    e.preventDefault()
-    erreurDate.innerHTML = "<font color='red'>Vous devez entrer votre date de naissance</font>"
+    erreurDate.textContent = "Vous devez entrer votre date de naissance"
     return false
 
   }
 }
 //Vérification tournois number
-const tournoiNbInvalide = (e) => {
+const tournoiNbInvalide = () => {
   let nbTournois = document.getElementById("quantity")
   let erreurNbTournois = document.getElementById("nbTournoisErrorMsg")
   let regexNbTournois = /^[0-9]{1}/
 
   if (!nbTournois.value) {
-    e.preventDefault()
-    erreurNbTournois.innerHTML = "<font color='red'>Veuillez saisir à combien de tournois vous avez participé</font>"
+    erreurNbTournois.textContent = "Veuillez saisir à combien de tournois vous avez participé"
     return false
   }
   if (nbTournois.value.match(regexNbTournois)) {
 
-    erreurNbTournois.innerHTML = ""
+    erreurNbTournois.textContent = ""
     return true
   }
   else {
-    e.preventDefault()
-    erreurNbTournois.innerHTML = "<font color='red'>ce champs doit contenir une valeur numérique </font>"
+    erreurNbTournois.textContent = "Ce champs doit contenir une valeur numérique"
     return false
 
   }
 }
-//Vérification tournois radios
-//Vérification tournois radios
-const tournoiInvalide = (e) => {
+//Vérification tournois radio
+const tournoiInvalide = () => {
   let tournois = document.querySelectorAll('input[type="radio"]')
   let erreurTournois = document.getElementById("tournoisErrorMsg")
   let radioSelectionne = false;
@@ -158,10 +146,9 @@ const tournoiInvalide = (e) => {
   tournois.forEach((tournoi) => {
     if (tournoi.checked) {
       radioSelectionne = true;
-      erreurTournois.innerHTML = ""
+      erreurTournois.textContent = ""
     } if (!radioSelectionne) {
-      e.preventDefault()
-      erreurTournois.innerHTML = "<font color='red'>Veuillez séléctionner un pays</font>"
+      erreurTournois.textContent = "Veuillez séléctionner un pays"
       radioSelectionne = false;
 
     }
@@ -169,36 +156,34 @@ const tournoiInvalide = (e) => {
   return radioSelectionne;
 
 }
-const condition = (e) => {
+const condition = () => {
   let conditionGenerale = document.getElementById("checkbox1")
   let conditionError = document.getElementById("conditionErrorMsg")
   if (conditionGenerale.checked) {
-    conditionError.innerHTML = ""
+    conditionError.textContent = ""
     return true
 
   } else {
-    e.preventDefault()
-    conditionError.innerHTML = "<font color='red'>Vous devez vérifier que vous acceptez les termes et conditions.</font>"
+    conditionError.textContent = "Vous devez vérifier que vous acceptez les termes et conditions."
     return false
 
   }
 }
 function showConfirmMessageOK() {
   form.style.display = "none";
-  let confirmMsgElement = document.getElementById("confirmation");
   confirmMsgElement.style.display = "block";
 }
 form.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-  let isPrenomValid = prenomInvalide(e)
-  let isNomValid = nomInvalide(e)
-  let isEmailValid = emailInvalide(e)
-  let isBirthDateValid = birthDateInvalid(e)
-  let isTournoiNbValid = tournoiNbInvalide(e)
-  let isTournoiValid = tournoiInvalide(e)
-  let isConditionValid = condition(e)
+  let isPrenomValid = prenomInvalide()
+  let isNomValid = nomInvalide()
+  let isEmailValid = emailInvalide()
+  let isBirthDateValid = birthDateInvalid()
+  let isTournoiNbValid = tournoiNbInvalide()
+  let isTournoiValid = tournoiInvalide()
+  let isConditionValid = condition()
   if (isPrenomValid && isNomValid && isEmailValid && isBirthDateValid && isTournoiNbValid && isTournoiValid && isConditionValid) {
-    //closeeModal(e)
     showConfirmMessageOK(e)
 
 
